a16affe9a4be73bd8cf38bd67ed8d87b78c9e845
ArturApp18 <artur.app19@gmail.com>
2022-12-27 16:30:55 +0100

    FirstCommit

---
+++ Assets/_Game/Scripts/PlayerInput.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace _Game.Scripts
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        [SerializeField] private PlayerMove _playerMove;
+        private void Start()
+        {
+            
+        }
+
+        
+        private void Update()
+        {
+            
+        }
+    }
+}
