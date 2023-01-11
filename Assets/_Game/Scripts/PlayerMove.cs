b47ad7b55e6b1930ca4c6b251057c1205a0a465b
RomanKapustinskyi <kapustinskyiroman@gmail.com>
2023-01-11 20:16:27 +0200

    refactor

--- Assets/_Game/Scripts/PlayerMove.cs
+++ Assets/_Game/Scripts/PlayerMove.cs
@@ -11,26 +11,47 @@ namespace _Game.Scripts
 
         private Vector3 _moveDirection;
 
+        private bool _isRotating;
+        private Quaternion _startRotation;
+
         private void FixedUpdate()
+        {
+            CalculateMoveDirrection();
+            UpdateRotation();
+            UpdatePosition();
+        }
+
+        private void CalculateMoveDirrection()
         {
             _moveDirection = new Vector3(_playerInput.horizontal * _speed, 0,
                 _playerInput.vertical * _speed);
+        }
 
-            if (_playerInput.isRunning)
+        private void UpdateRotation()
+        {
+            if (_playerInput.isRotating)
             {
-                _rigidbody.velocity = _moveDirection;
+                if (!_isRotating)
+                {
+                    _startRotation = transform.rotation;
+                    _isRotating = true;
+                }
+
+                Quaternion lookDirection = transform.rotation * Quaternion.LookRotation(_moveDirection);
+                transform.rotation = Quaternion.Lerp(transform.rotation, lookDirection, 0.05f);
             }
             else
             {
-                _playerInput.isRunning = false;
+                _isRotating = false;
             }
+        }
 
-            if (_playerInput.isRotating)
-            {
-                Vector3 lookDirection = new Vector3(_moveDirection.x, 0,
-                    _moveDirection.z);
-                transform.rotation = Quaternion.LookRotation(lookDirection);
-            }
+        private void UpdatePosition()
+        {
+            if (_playerInput.isRunning)
+                _rigidbody.velocity = transform.forward * _speed;
+            else
+                _rigidbody.velocity = Vector3.zero;
         }
     }
 }
